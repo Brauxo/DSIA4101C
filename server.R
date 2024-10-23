6ee0f8b320fbb6f5834b04b42a230367cf97f8c9
cambierelliot <elliot.cambier@edu.esiee.fr>
2024-10-23 11:26:50 +0200

    decomposition en 3 fichier -ELLIOT

---
+++ server.R
@@ -0,0 +1,13 @@
+library(shiny)
+library(leaflet)
+
+# Server logic
+server <- function(input, output, session) {
+  
+  # Render a blank map
+  output$map <- renderLeaflet({
+    leaflet() %>%
+      addTiles(urlTemplate = "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png") %>%
+      setView(lng = 1.888334, lat = 46.603354, zoom = 6)  # Center on France
+  })
+}
---
+++ ui.R
@@ -0,0 +1,68 @@
+library(shiny)
+library(shinydashboard)
+library(leaflet)  # Ensure leaflet is loaded
+
+ui <- dashboardPage(
+  dashboardHeader(
+    title = tags$div(
+      style = "width: 100%; text-align: center;",
+      "Chemin de fer en France"
+    ),
+    titleWidth = "100%"
+  ),
+  
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Histogramme", tabName = "histogram", 
+               icon = icon("chart-simple")),
+      menuItem("Cartographie", tabName = "map", 
+               icon = icon("map-location")),
+      menuItem("Pie Chart", tabName = "piechart", 
+               icon = icon("chart-pie")),
+      menuItem("Distribution", tabName = "distribution", 
+               icon = icon("th"))
+    )
+  ),
+  
+  dashboardBody(
+    tags$head(
+      tags$style(HTML("
+        body {
+          min-width: 800px;
+          min-height: 600px;
+        }
+      "))
+    ),
+    
+    tabItems(
+      tabItem(tabName = "histogram",
+              fluidRow(
+                box(
+                  title = "Sélectionnez les paramètres pour l'histogramme",
+                  solidHeader = TRUE,
+                  status = "primary"
+                )
+              )
+      ),
+      
+      # Cartographie tab where the map is displayed
+      tabItem(tabName = "map",
+              fluidRow(
+                selectInput(
+                  inputId = "lgv_option",
+                  label = "Choisissez une option:",
+                  choices = list("Toutes les lignes" = "both", "LGV" = "LGV", "Lignes classiques" = "classique", "LGV complété" = "LGVC"),
+                  selected = "both"
+                ),
+                box(
+                  width = 9,  # Adjust width as needed
+                  status = "primary",
+                  solidHeader = TRUE,
+                  leafletOutput("map", height = "600px", width = "100%"),  # Full width map with padding
+                  id = "map-box"
+                )
+              )
+      )
+    )
+  )
+)
